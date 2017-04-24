2d0118dc1c762fedf7722f328c5799b052ba87c2
physica <physica@outlook.com>
2017-04-24 16:49:54 +0800

    ajaxFileUpload

--- WebApplication2/WebForm1.aspx
+++ WebApplication2/WebForm1.aspx
@@ -8,10 +8,20 @@
     <title></title>
 </head>
 <body>
-    <form id="form1" runat="server">
-    <div>
     
+    <form id="form1" runat="server">
+        
+    <div><asp:scriptmanager runat="server"></asp:scriptmanager>
+        
+        <asp:Button ID="Button1" runat="server" Text="Button" />
+        <ajaxToolkit:ModalPopupExtender ID="ModalPopupExtender1" runat="server" PopupControlID="pan1" TargetControlID="Button1" CancelControlID="cancelbutton"></ajaxToolkit:ModalPopupExtender>
+        <asp:Panel ID="pan1" style="display:none" runat="server">
+            hahah
+            <asp:Button ID="cancelbutton" Text="cancel" runat="server" />
+        </asp:Panel>
+        <ajaxToolkit:AjaxFileUpload ID="AjaxFileUpload1" runat="server" ChunkSize="409600" OnDataBinding="AjaxFileUpload1_DataBinding" OnUploadComplete="AjaxFileUpload1_UploadComplete1" OnUploadCompleteAll="AjaxFileUpload1_UploadCompleteAll" OnUploadStart="AjaxFileUpload1_UploadStart" />
     </div>
     </form>
 </body>
+
 </html>
--- WebApplication2/WebForm1.aspx.cs
+++ WebApplication2/WebForm1.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.IO;
 
 namespace WebApplication2
 {
@@ -14,6 +15,31 @@ namespace WebApplication2
             //windows的注释！
 			int a = 0;
             int b = a;
+        }
+
+        protected void AjaxFileUpload1_UploadComplete(object sender, AjaxControlToolkit.AjaxFileUploadEventArgs e)
+        {
+            FileInfo f = new FileInfo(e.FileName);
+        }
+
+        protected void AjaxFileUpload1_UploadStart(object sender, AjaxControlToolkit.AjaxFileUploadStartEventArgs e)
+        {
+
+        }
+
+        protected void AjaxFileUpload1_UploadCompleteAll(object sender, AjaxControlToolkit.AjaxFileUploadCompleteAllEventArgs e)
+        {
+
+        }
+
+        protected void AjaxFileUpload1_UploadComplete1(object sender, AjaxControlToolkit.AjaxFileUploadEventArgs e)
+        {
+
+        }
+
+        protected void AjaxFileUpload1_DataBinding(object sender, EventArgs e)
+        {
+
         }
     }
 }
